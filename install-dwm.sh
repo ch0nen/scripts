2bde21248d6019ecbe84dead146a6707cd764cf6
Joakim Andersson <jocke@bulle.nu>
2024-02-27 13:23:42 +0100

    Update install-dwm.sh

--- install-dwm.sh
+++ install-dwm.sh
@@ -30,16 +30,17 @@ mkdir -p "$PATCHES_DIR"
 # List of patches to apply
 declare -A PATCHES=(
   ["alwayscenter"]="https://dwm.suckless.org/patches/alwayscenter/dwm-alwayscenter-20200625-f04cac6.diff"
-  ["attachbottom"]="https://dwm.suckless.org/patches/attachbottom/dwm-attachbottom-6.3.diff"
-  ["cool_autostart"]="https://dwm.suckless.org/patches/cool_autostart/dwm-cool-autostart-6.2.diff"
-  ["fakefullscreen"]="https://dwm.suckless.org/patches/fakefullscreen/dwm-fakefullscreen-20210714-138b405.diff"
-  ["fullgaps"]="https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-6.4.diff"
-  ["hide_vacant_tags"]="https://dwm.suckless.org/patches/hide_vacant_tags/dwm-hide_vacant_tags-6.3.diff"
-  ["preserveonrestart"]="https://dwm.suckless.org/patches/preserveonrestart/dwm-preserveonrestart-6.3.diff"
-  ["status2d"]="https://dwm.suckless.org/patches/status2d/dwm-status2d-6.3.diff"
-  ["titlecolor"]="https://dwm.suckless.org/patches/titlecolor/dwm-titlecolor-20210815-ed3ab6b4.diff"
-  ["bottomstack"]="https://dwm.suckless.org/patches/bottomstack/dwm-bottomstack-6.1.diff"
 )
+
+#  ["attachbottom"]="https://dwm.suckless.org/patches/attachbottom/dwm-attachbottom-6.3.diff"
+#  ["cool_autostart"]="https://dwm.suckless.org/patches/cool_autostart/dwm-cool-autostart-6.2.diff"
+#  ["fakefullscreen"]="https://dwm.suckless.org/patches/fakefullscreen/dwm-fakefullscreen-20210714-138b405.diff"
+#  ["fullgaps"]="https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-6.4.diff"
+#  ["hide_vacant_tags"]="https://dwm.suckless.org/patches/hide_vacant_tags/dwm-hide_vacant_tags-6.3.diff"
+#  ["preserveonrestart"]="https://dwm.suckless.org/patches/preserveonrestart/dwm-preserveonrestart-6.3.diff"
+#  ["status2d"]="https://dwm.suckless.org/patches/status2d/dwm-status2d-6.3.diff"
+#  ["titlecolor"]="https://dwm.suckless.org/patches/titlecolor/dwm-titlecolor-20210815-ed3ab6b4.diff"
+#  ["bottomstack"]="https://dwm.suckless.org/patches/bottomstack/dwm-bottomstack-6.1.diff"
 
 # Download and apply patches
 cd "$DWM_DIR"
